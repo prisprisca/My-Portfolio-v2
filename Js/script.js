880c8a138eab0dcf92d04a57317552b05793673f
prisprisca <prisca20bamwireku@gmail.com>
2022-04-18 19:40:29 +0300

    modified content in home section

--- Js/script.js
+++ Js/script.js
@@ -1,6 +1,6 @@
 // typing 
 let typed = new Typed(".typing",{
-    strings:["Web Developer","A Coding Instructor","A CopyWriter","An Ethical Hacker","A Web Developer"],
+    strings:["Web Developer","A Coding Instructor","An Ethical Hacker","A Web Developer"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
