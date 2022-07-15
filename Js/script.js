ee59f2a56bc824806b208fc1916a6edae35c18c2
prisprisca <prisca20bamwireku@gmail.com>
2022-07-15 07:49:35 +0300

    added link to my cv

--- Js/script.js
+++ Js/script.js
@@ -1,6 +1,6 @@
 // typing 
 let typed = new Typed(".typing",{
-    strings:["Web Developer","A Learning Facilitator","An Ethical Hacker","A Web Developer"],
+    strings:["Web Developer","A Learning Facilitator","An Aspiring Ethical Hacker","A Web Developer"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
